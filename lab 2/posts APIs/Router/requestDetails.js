52d6850cd50e729ecd25db8012087e63beac0927
Mohammed tareq <Mohtareq1999m@gmail.com>
2025-06-14 08:37:00 +0300

    Implement lab 2 functionality

--- lab 2/posts APIs/Router/requestDetails.js
+++
@@ -1,7 +0,0 @@
-const requestDetails = (req, res, next) => {
-    const route = req.originalUrl;
-    const method = req.method;
-    const timestamp = new Date().toISOString();    next();
-};
-
-export default requestDetails; 
